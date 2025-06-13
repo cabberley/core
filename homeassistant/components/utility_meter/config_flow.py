082e9f8a6da743aaaf01d8513459357e418d11f0
Chris Abberley <26394346+cabberley@users.noreply.github.com>
2025-06-13 16:04:59 +1000

    Update config_flow.py

--- homeassistant/components/utility_meter/config_flow.py
+++ homeassistant/components/utility_meter/config_flow.py
@@ -3,6 +3,7 @@
 from __future__ import annotations
 
 from collections.abc import Mapping
+from datetime import timedelta
 from typing import Any, cast
 
 import voluptuous as vol
@@ -64,7 +65,69 @@ async def _validate_config(
         vol.Unique()(user_input[CONF_TARIFFS])
     except vol.Invalid as exc:
         raise SchemaFlowError("tariffs_not_unique") from exc
-
+    offset_timedelta = timedelta(seconds=0)
+    try:
+        offset_timedelta = timedelta(seconds = user_input[CONF_METER_OFFSET]["seconds"] + \
+            user_input[CONF_METER_OFFSET]["minutes"] * 60 + \
+            user_input[CONF_METER_OFFSET]["hours"] * 3600 + \
+            user_input[CONF_METER_OFFSET]["days"] * 86400)
+    except vol.Invalid as exc:
+        raise SchemaFlowError("invalid_meter_offset") from exc
+    if user_input[CONF_METER_TYPE] == EVERY_FIVE_MINUTES:
+        if offset_timedelta >= timedelta(minutes=5):
+            raise SchemaFlowError(
+                f"Offset for {CONF_METER_TYPE} must be less than 5 minutes"
+            )
+    elif user_input[CONF_METER_TYPE] == QUARTER_HOURLY:
+        if user_input[CONF_METER_OFFSET] >= timedelta(minutes=15):
+            raise vol.Invalid(
+                f"Offset for {CONF_METER_TYPE} must be less than 15 minutes"
+            )
+    elif user_input[CONF_METER_TYPE] == HALF_HOURLY:
+        if user_input[CONF_METER_OFFSET] >= timedelta(minutes=30):
+            raise vol.Invalid(
+                f"Offset for {CONF_METER_TYPE} must be less than 30 minutes"
+            )
+    elif user_input[CONF_METER_TYPE] == HOURLY:
+        if user_input[CONF_METER_OFFSET] >= timedelta(hours=1):
+            raise vol.Invalid(
+                f"Offset for {CONF_METER_TYPE} must be less than 1 hour"
+            )
+    elif user_input[CONF_METER_TYPE] == DAILY:
+        if user_input[CONF_METER_OFFSET] >= timedelta(days=1):
+            raise vol.Invalid(
+                f"Offset for {CONF_METER_TYPE} must be less than 1 day"
+            )
+    elif user_input[CONF_METER_TYPE] == WEEKLY:
+        if user_input[CONF_METER_OFFSET] >= timedelta(weeks=1):
+            raise vol.Invalid(
+                f"Offset for {CONF_METER_TYPE} must be less than 1 week"
+            )
+    elif user_input[CONF_METER_TYPE] == MONTHLY:
+        if user_input[CONF_METER_OFFSET] >= timedelta(days=28):
+            raise vol.Invalid(
+                f"Offset for {CONF_METER_TYPE} must be less than 31 days"
+            )
+    elif user_input[CONF_METER_TYPE] == BIMONTHLY:
+        if user_input[CONF_METER_OFFSET] >= timedelta(days=59):
+            raise vol.Invalid(
+                f"Offset for {CONF_METER_TYPE} must be less than 59 days"
+            )
+    elif user_input[CONF_METER_TYPE] == QUARTERLY:
+        if user_input[CONF_METER_OFFSET] >= timedelta(days=90):
+            raise vol.Invalid(
+                f"Offset for {CONF_METER_TYPE} must be less than 90 days"
+            )
+    elif user_input[CONF_METER_TYPE] == HALF_YEARLY:
+        if user_input[CONF_METER_OFFSET] >= timedelta(days=183):
+            raise vol.Invalid(
+                f"Offset for {CONF_METER_TYPE} must be less than 183 days"
+            )
+    elif user_input[CONF_METER_TYPE] == YEARLY:
+        if user_input[CONF_METER_OFFSET] >= timedelta(days=365):
+            raise vol.Invalid(
+                f"Offset for {CONF_METER_TYPE} must be less than 365 days"
+            )
     return user_input
 
 
@@ -94,12 +157,11 @@ CONFIG_SCHEMA = vol.Schema(
                 options=METER_TYPES, translation_key=CONF_METER_TYPE
             ),
         ),
-        vol.Required(CONF_METER_OFFSET, default=0): selector.NumberSelector(
-            selector.NumberSelectorConfig(
-                min=0,
-                max=28,
-                mode=selector.NumberSelectorMode.BOX,
-                unit_of_measurement="days",
+        vol.Required(CONF_METER_OFFSET, default=None): selector.DurationSelector(
+            selector.DurationSelectorConfig(
+                enable_day= True,
+                enable_millisecond=False,
+                allow_negative=False,
             ),
         ),
         vol.Required(CONF_TARIFFS, default=[]): selector.SelectSelector(
@@ -134,7 +196,7 @@ OPTIONS_FLOW = {
 class ConfigFlowHandler(SchemaConfigFlowHandler, domain=DOMAIN):
     """Handle a config or options flow for Utility Meter."""
 
-    VERSION = 2
+    VERSION = 3
 
     config_flow = CONFIG_FLOW
     options_flow = OPTIONS_FLOW
